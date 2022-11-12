54f915600705c5846ee09df4261d6298db13b1a0
raqueljoseph <raquelajoseph@gmail.com>
2022-11-12 14:21:11 -0500

    add index.html to root

--- server/app.py
+++ server/app.py
@@ -4,26 +4,22 @@ from twilio.rest import Client
 
 app = Flask(__name__,template_folder='../client/templates')
 
-
 @app.route('/')
 def index():
-    return 'Hello, World!'
-
+    return render_template('index.html')
 
 @app.route('/landing')
 def landing():
     return render_template('landing.html')
 
-# client = Client(account_sid, auth_token)
+client = Client(account_sid, auth_token)
 
-# client = Client(account_sid, auth_token)
-
-# def sendMessage(number,text):
-#     message = client.messages.create(
-#         to="+1" + number, #assume all numbers are US based
-#         from_="+14782495437", #automatic twilio number
-#         body=text)  
-#     # print(message.sid)
+def sendMessage(number,text):
+    message = client.messages.create(
+        to="+1" + number, #assume all numbers are US based
+        from_="+14782495437", #automatic twilio number
+        body=text)  
+    # print(message.sid)
 
 # sendMessage("2018381407","working")
 
