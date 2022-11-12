bcf65f6bd3b3955cdf683803a5fe1841250f152c
Ayca Solmaz <74674789+aycasolmaz@users.noreply.github.com>
2022-11-12 13:17:28 -0500

    firebase auth

--- server/app.py
+++ server/app.py
@@ -8,14 +8,19 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-client = Client(account_sid, auth_token)
+# # Your Account SID from twilio.com/console
+# account_sid = "AC094bc9651cf72d8d19434194b5f638b7"
+# # Your Auth Token from twilio.com/console
+# auth_token  = "3ee1da683abae91d2fee555033c331e3"
 
-def sendMessage(number,text):
-    message = client.messages.create(
-        to="+1" + number, #assume all numbers are US based
-        from_="+14782495437", #automatic twilio number
-        body=text)  
-    # print(message.sid)
+# client = Client(account_sid, auth_token)
 
-sendMessage("2018381407","working")
+# def sendMessage(number,text):
+#     message = client.messages.create(
+#         to="+1" + number, #assume all numbers are US based
+#         from_="+14782495437", #automatic twilio number
+#         body=text)  
+#     # print(message.sid)
+
+# sendMessage("2018381407","working")
 
