3221513290633a11df1bd5bbe456dbec7c2b3929
raqueljoseph <raquelajoseph@gmail.com>
2022-11-12 12:24:44 -0500

    add sendMessage function

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from twilio.rest import Client
 
 app = Flask(__name__)
 
@@ -8,4 +9,19 @@ def hello():
     return 'Hello, World!'
 
 
+# Your Account SID from twilio.com/console
+account_sid = "AC094bc9651cf72d8d19434194b5f638b7"
+# Your Auth Token from twilio.com/console
+auth_token  = "a010edbcca3a4f6fd515c69370f140f4"
+
+client = Client(account_sid, auth_token)
+
+def sendMessage(number,text):
+    message = client.messages.create(
+        to="+1" + number, #assume all numbers are US based
+        from_="+14782495437", #automatic twilio number
+        body=text)  
+    # print(message.sid)
+
+sendMessage("2018381407","working")
 
